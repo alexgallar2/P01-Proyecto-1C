ca477f997ef62f4bde37750db2bfeb6e1cbab095
alexgallar2 <93150064+alexgallar2@users.noreply.github.com>
2022-11-13 16:17:24 +0100

    segunda entrega (por acabar)

--- src/test.py
+++ src/test.py
@@ -1,7 +1,34 @@
 from funciones import *
 
+def test_filtra_por_pais(archivo, pais):
+    filtrado_pais= filtra_por_pais(archivo, pais)
+    print(filtrado_pais)
+
+def test_lector_seismos(archivo):
+        for r in seismos:
+            print(r)
+
+def test_promedio_muertes(archivo):
+    prom= promedio_muertes(archivo)
+    print(prom)
+
+def test_ordenar_por_fecha(archivo, n, tsunami):
+    orden = ordenar_por_fecha(archivo, n, tsunami)
+    print(orden)
+
+def test_maximos_heridos(archivo):
+    res = maximos_heridos(archivo)
+    print(res)
+
+def test_pais_mas_frecuente(archivo):
+    res = pais_mas_frecuente(archivo)
+    print (res) 
 
 if __name__ == "__main__":
-    seismos = lector_seismos("C:/Users/ALEX/proy_entreg/P01-Proyecto-1C/data/seismo.csv")
-    for r in seismos:
-        print(r)
+    seismos = lector_seismos("../P01-Proyecto-1C/data/seismo.csv")
+    #test_lector_seismos(seismos)
+    #test_filtra_por_pais(seismos, 'CHINA')
+    #test_promedio_muertes(seismos)
+    #test_ordenar_por_fecha(seismos, 4, True)
+    #test_maximos_heridos(seismos)
+    #test_pais_mas_frecuente(seismos)
