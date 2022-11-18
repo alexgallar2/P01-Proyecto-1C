c4aced4a27ec978043185f3c693788e3a2c729ab
alexgallar2 <93150064+alexgallar2@users.noreply.github.com>
2022-11-18 12:00:18 +0100

    segunda entrega proyecto

--- src/funciones.py
+++ src/funciones.py
@@ -1,7 +1,9 @@
 from collections import namedtuple
 import csv
 from datetime import datetime
+import statistics
 import funciones_aux
+from statistics import *
 
 seismo = namedtuple("seismo", "ID,TSUNAMI,FECHA,HORA,PAIS,LATITUD,LONGITUD,MUERTES,HERIDOS")
 def lector_seismos(archivo):
@@ -22,3 +24,30 @@ def lector_seismos(archivo):
             lista.append(tupla)
         return lista
 
+def filtra_por_pais(archivo, pais):
+    return [ t for t in archivo if t.PAIS==pais]
+
+def promedio_muertes(archivo):
+    lista =[]
+    for t in archivo:
+        lista.append(t.MUERTES)
+        prom = mean(lista)
+    return prom
+
+def ordenar_por_fecha(archivo, n, tsunami = None):
+    if tsunami != None:
+        filtrado = [ t for t in archivo if t.TSUNAMI==tsunami]
+        res = sorted(filtrado, key=lambda t:t.FECHA, reverse=False)[:n]
+    else:
+        res = sorted(archivo, key=lambda t:t.FECHA, reverse=False)[:n]
+    return res
+
+def maximos_heridos(archivo):
+    maximo = max(archivo, key=lambda t:t.HERIDOS)
+    res = [t for t in archivo if t.HERIDOS==maximo.HERIDOS]
+    return res
+    
+def pais_mas_frecuente(archivo):
+    freq = max(archivo, key = lambda t:t.PAIS)
+    res = [ t.PAIS for t in archivo if t.PAIS==freq.PAIS]
+    return set(res)
--- src/funciones_aux.py
+++ src/funciones_aux.py
@@ -1,17 +1,16 @@
-from datetime import *
+
 
 def parse_muertes(cadena):
     if cadena:
         muertes_parseadas= int(cadena)
         return muertes_parseadas
     else:
-        muertes_vacias = None
+        muertes_vacias = 0
         return muertes_vacias
 def parse_heridos(cadena2):
     if cadena2:
         heridos_parseados= int(cadena2)
         return heridos_parseados
     else:
-        heridos_vacios = None
+        heridos_vacios = 0
         return heridos_vacios
-
